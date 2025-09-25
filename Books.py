74ee4a660c9e3d07fd8de9fdaaf8aea191bcbf6e
iareebahmad <m.areeb.ahmad@gmail.com>
2025-09-25 21:29:52 +0530

    PUT Method

--- Books.py
+++ Books.py
@@ -75,3 +75,11 @@ async def read_category_by_query(book_author :str, category :str):
 @app.post("/books/create_book")
 async def create_book(new_book=Body()):
     BOOKS.append(new_book)
+
+# PUT Method: Used to Update Data, can have body like post.
+# Changing category of author
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('Title').casefold() == updated_book.get('Title').casefold():
+            BOOKS[i] = updated_book
