33e7b77f779dcd0392b3f58d91ee883e449dc051
iareebahmad <m.areeb.ahmad@gmail.com>
2025-10-01 01:34:18 +0530

    HTTPException and status codes from starlette

--- books2.py
+++ books2.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI, Body,Path,Query  #Path is used to validate path parameter ans Query for validating query params
+from fastapi import FastAPI, Body,Path,Query, HTTPException  #Path is used to validate path parameter ans Query for validating query params
 from pydantic import BaseModel, Field
 from typing import Optional
+from starlette import status        #FastAPI is built using starlette, hence installing FastAPI means starlette is automatically installed
+
 app = FastAPI()
 
 class Book:
@@ -22,7 +24,7 @@ class Book:
 
 # Book Request from Pydantic
 class BookRequest(BaseModel):
-    #id : Optional[int] = None  # means it can either be null or zero. No need to pass any value in post req for id, id gets assigned because of py fn find_book_id()
+    #Id : Optional[int] = None  # means it can either be null or zero. No need to pass any value in post req for id, id gets assigned because of py fn find_book_id()
     #or
     id : Optional[int] = Field(description='ID is not needed on create', default= None)
     title :str = Field(min_length=3)
@@ -59,7 +61,7 @@ BOOKS = [
 
 # API Endpoints
 # Read all books
-@app.get("/books")
+@app.get("/books", status_code=status.HTTP_200_OK)
 async def read_all_books():
     return BOOKS
 
@@ -77,7 +79,7 @@ To put a check on this, we need Pydantic.
 
 # Add a new book using Pydantic for data validation
 # #.model_dump() converts the request into a normal dictionary (like {"title": "Harry Potter", "author": "Rowling"}).
-@app.post("/create-book")
+@app.post("/create-book", status_code=status.HTTP_201_CREATED)
 async def create_book(book_request :BookRequest):
     new_book = Book(**book_request.model_dump()) # Book() creates an object of class Book and .model_dump converts the request to a normal dictionary
     BOOKS.append(find_book_id(new_book))
@@ -94,14 +96,15 @@ def find_book_id(book: Book):
 
 # API Endpint
 # Find a book based on Book id
-@app.get("/books/{book_id}")
+@app.get("/books/{book_id}", status_code=status.HTTP_200_OK)
 async def read_book(book_id: int = Path(gt=0)):         #Validating path params
     for book in BOOKS:
         if book.id == book_id:
             return book
+    raise HTTPException(status_code=404, detail="Item Not Found")
 
 # Fetch books by rating
-@app.get("/books/")                                         # Validating query param
+@app.get("/books/", status_code=status.HTTP_200_OK)                                         # Validating query param
 async def read_book_by_rating(book_rating :int = Query(gt=0, lt=6)):
     books_to_return = []
     for book in BOOKS:
@@ -110,26 +113,50 @@ async def read_book_by_rating(book_rating :int = Query(gt=0, lt=6)):
     return books_to_return
 
 # Update an exisiting book by Book id
-@app.put("/books/update_book")
+@app.put("/books/update_book",status_code=status.HTTP_204_NO_CONTENT)
 async def update_book(book :BookRequest):
+    book_changed = False
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book.id:
             BOOKS[i] = book
+            book_changed = True
+    if not book_changed:
+        raise HTTPException(status_code=404,detail="Item Not Found")
 
 
 # Delete a book
-@app.delete("/books/{book_id}")
+@app.delete("/books/{book_id}",status_code=status.HTTP_204_NO_CONTENT)
 async def delete_book(book_id :int = Path(gt=0)):
+    book_changed = False
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book_id:
             BOOKS.pop(i)
+            book_changed = True
             break
+    if not book_changed:
+        raise HTTPException(status_code=404,detail="Item Not Found")
 
 # Fetch book by publication date
-@app.get("/books/find_book/")
+@app.get("/books/find_book/", status_code=status.HTTP_200_OK)
 async def book_by_date(book_date: int = Query(gt=1995, lt=2040)):       # Validating query param
     booklist_by_date = []
     for book in BOOKS:
         if book.published_date == book_date:
             booklist_by_date.append(book)
     return booklist_by_date
+"""
+Status Code: Help Client to understand what happened on the server side application. They are internatonal standards on how a client/server should HANDLE THE RESULT OF A REQUEST
+- 1XX: Information Response: Request Processing
+- 2XX: Success: Request Successfully Complete
+    - 200: OK
+    - 201: Created
+    - 204: No Content (Used with PUT: Not returning anything but it changed something behind scenes on the server)
+- 3XX: Redirection: Further Action must be complete
+- 4XX: Client Errors: An error was caused by the client
+    - 400: Bad Request : Used for invalid request methods
+    - 401: Unauthorized
+    - 404: Not Found
+    - 422: Unprocessable Entity: Some sematic error in the request by the client
+- 5XX: Server Errors: An error occurred in the server
+    - 500 : Internal Server Error
+"""
