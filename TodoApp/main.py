be0fcfea28e6eac01f0bc183737be49f4c078a0c
iareebahmad <m.areeb.ahmad@gmail.com>
2025-10-04 16:54:29 +0530

    get method

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,8 +1,25 @@
 #main.py
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Depends
+from fastapi.params import Depends
+from models import Todos
 import models
-from database import engine
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+async def read_all(db: Annotated[Session, Depends(get_db())]):
+    return db.query(Todos).all()
